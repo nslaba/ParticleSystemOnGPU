e74f40c2d252a2d18db8c4c609a8bfa22df8ebde
Natalie Slaba <slaba.natalie@gmail.com>
2023-03-11 15:40:32 -0800

    display is working

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,61 @@
 #include "ShaderHandler.h"
+#include <../../../../../../PROJECT/project/GPUParticles/GPUParticles/external/GLFW/glfw-3.3.8.bin.WIN32/include/GLFW/glfw3.h>
+#include <iostream>
 
 int main() {
+
+	/*STEP 1: deal with display*/
+	
+	// Initialize GLFW
+	if (!glfwInit())
+	{
+		std::cout << "Failed to initialize GLFW" << std::endl;
+		return -1;
+	}
+
+	// Set OpenGL version to 4.6
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+
+	// Create a window
+	GLFWwindow* window = glfwCreateWindow(800, 600, "My Window", nullptr, nullptr);
+	if (!window)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Make the context of the window the current one
+	glfwMakeContextCurrent(window);
+
+	// Initialize GLAD to load OpenGL function pointers
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	// Set the screen color to green
+	glClearColor(0.0f, 1.0f, 0.0f, 1.0f);
+
+	// Main loop
+	while (!glfwWindowShouldClose(window))
+	{
+		// Clear the color buffer
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Swap the front and back buffers
+		glfwSwapBuffers(window);
+
+		// Poll for and process events
+		glfwPollEvents();
+
+	}
+
+	// Clean up GLFW
+	glfwTerminate();
 
 	return 0;
 }
